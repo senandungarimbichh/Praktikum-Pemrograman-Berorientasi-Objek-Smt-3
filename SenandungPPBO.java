5306a9841a119090fa16e3bf8864b70c3458ad29
senandungarimbichh <senandungmarimby95@gmail.com>
2025-08-31 22:09:29 +0700

    Add files via upload

---
+++ SenandungPPBO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.senandungppbo;
+
+/**
+ *
+ * @author Senandung Marimby
+ */
+public class SenandungPPBO {
+
+    public static void main(String[] args) {
+        System.out.println("Nama    : Senandung Marimby Zahira Malika");
+        System.out.println("NPM     : 2430506050");
+        System.out.println("Alamat  : Dumpoh, Magelang");
+        System.out.println("No_HP   : 0878989806");
+    }
+}
